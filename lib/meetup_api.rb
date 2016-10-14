2f6897ed7063a35cc8b2888aa3fe95be0bc78f19
isaacmtz90 <imtz90b@gmail.com>
2016-10-14 17:49:32 +0800

    Fixed method for getting events

--- lib/meetup_api.rb
+++ lib/meetup_api.rb
@@ -9,10 +9,12 @@ module Meetup
     VERSIONED_API_URL = URI.join(API_URL, "#{API_VERSION}/")
     CREDENTIALS = YAML.load(File.read('../config/credentials.yml'))
 
+    # Grabs the api ket from config file inside the config directory
     def initialize
       @access_key = CREDENTIALS['api_key']
     end
 
+    # Gets cities based on country code (ex. tw)
     def get_cities(country_code)
       api_url = URI.join(VERSIONED_API_URL, '/cities/')
       puts api_url
@@ -22,17 +24,20 @@ module Meetup
                                            key: @access_key })
       response = JSON.parse(cities_response.to_s)
       add_log(response, "cities_#{country_code}")
+      response
     end
 
-    def get_events(lon, lat)
+    # Gets events based on the location. Place var just for fixtures
+    def get_events(place, lat, lon)
       api_url = URI.join(API_URL, '/find/events/')
-      events_response = response HTTP.get(api_url,
-                                          params: { lon: lon,
-                                                    lat: lat,
-                                                    signed: true,
-                                                    key: @access_key })
+      events_response = HTTP.get(api_url,
+                                 params: { lon: lon,
+                                           lat: lat,
+                                           signed: true,
+                                           key: @access_key })
       response = JSON.parse(events_response.to_s)
       puts response
+      add_log(response, "events_#{place}")
     end
 
     def add_log(response, filename)
--- lib/testing.rb
+++ lib/testing.rb
@@ -1,3 +1,7 @@
 require_relative 'meetup_api'
 @meetup_api = Meetup::MeetupApi.new
-@meetup_api.get_cities('tw')
+cities = @meetup_api.get_cities('tw')['results']
+c = cities[0]
+
+events = @meetup_api.get_events(c['city'], c['lat'], c['lon'])
+puts events
