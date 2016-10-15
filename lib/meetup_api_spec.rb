c72a4df373264b873a044506d3599980dc16f404
ryebra <ryebra@gmail.com>
2016-10-15 22:38:23 +0800

    Spec Structure

--- lib/meetup_api_spec.rb
+++ lib/meetup_api_spec.rb
@@ -1,21 +1,43 @@
-# CODE: Make a library project from your earlier script
-# Start a new git branch (name it api_library)
-# Organize your files into folders (config, lib, spec/fixtures, etc.) and update your .gitignore
-# Write a spec file to test a new Module::Class that you will create as your library
-# Write your new Module::Class libraries in multiple files in lib folder, and pass your tests
-# Be sure to make a git commit at this stage and merge your branch into master
 
 require 'minitest/autorun'
 require 'minitest/rg'
 
 require_relative 'meetup_api'
 
-@meetup_api = Meetup::MeetupApi.new
-cities = @meetup_api.get_cities('tw')['results']
-c = cities[0]
+CREDENTIALS = YAML.load(File.read('../config/credentials.yml'))
+FB_RESPONSE = YAML.load(File.read('spec/fixtures/fb_response.yml'))
+RESULTS = YAML.load(File.read('spec/fixtures/results.yml'))
 
-events = @meetup_api.get_events(c['city'], c['lat'], c['lon'])
-puts events
+describe 'MeetUp Api tests' do
+  it 'should return the cities using the country_code' do
+    tw
+  end
+  it 'should save the results of get_cities to "cities country_code.yml"' do
+    cities_tw.yml
+  end
+  it 'should return the events using the City' do
+    Taipei
+  end
+  it 'should save the results of get_events to "events City.yml"' do
+    cities_tw.yml
+  end
+  it 'should return the groups using the location_raw_text' do
+    TaipeiCity
+  end
+  it 'should save the results of get_groups to "cities country_code.yml"' do
+    cities_tw.yml
+  end
 
-groups = @meetup_api.get_groups('tw', 'Taipei City')
-puts groups
+end
+
+  @meetup_api = Meetup::MeetupApi.new
+  cities = @meetup_api.get_cities('tw')['results']
+  c = cities[0]
+
+  events = @meetup_api.get_events(c['city'], c['lat'], c['lon'])
+  puts events
+
+  groups = @meetup_api.get_groups('tw', 'Taipei City')
+  puts groups
+
+end
