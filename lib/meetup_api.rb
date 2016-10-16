025ad4bb335a95771ace2daeac3b1c85d819581b
isaacmtz90 <imtz90b@gmail.com>
2016-10-16 17:58:05 +0800

    fixing the file creation, fking whitespaces...

--- lib/meetup_api.rb
+++ lib/meetup_api.rb
@@ -7,18 +7,15 @@ module Meetup
     API_VERSION = '2'.freeze
     LOG_LOCATION = '../spec/fixtures/'.freeze
     VERSIONED_API_URL = URI.join(API_URL, "#{API_VERSION}/")
-    CREDENTIALS = YAML.load(File.read('../config/credentials.yml'))
 
     # Grabs the api ket from config file inside the config directory
-    def initialize
-      @access_key = CREDENTIALS.first['api_key']
+    def initialize(credentials)
+      @access_key = credentials.first['api_key']
     end
 
     # Gets cities based on country code (ex. tw)
     def get_cities(country_code)
       api_url = URI.join(VERSIONED_API_URL, '/cities/')
-      puts
-      puts api_url
       cities_response = HTTP.get(api_url,
                                  params: { country: country_code,
                                            signed: true,
@@ -31,36 +28,30 @@ module Meetup
     # Gets events based on the location. Place var just for fixtures
     def get_events(place, lat, lon)
       api_url = URI.join(API_URL, '/find/events/')
-      puts
-      puts api_url
       events_response = HTTP.get(api_url,
                                  params: { lon: lon,
                                            lat: lat,
                                            signed: true,
                                            key: @access_key })
       response = JSON.parse(events_response.to_s)
-      puts response
       add_log(response, "events_#{place}")
     end
 
     # Finds groups based on a location text query
     def get_groups(country_code, location_raw_text)
       api_url = URI.join(API_URL, '/find/groups')
-      puts
-      puts api_url
       groups_response = HTTP.get(api_url,
                                  params: { country: country_code,
                                            fallback_suggestions: 'true',
                                            location: location_raw_text,
                                            key: @access_key })
       response = JSON.parse(groups_response.to_s)
-      puts response
       add_log(response, "groups_at_#{location_raw_text}")
     end
 
     def add_log(response, filename)
-      filepath = "#{LOG_LOCATION} #{filename}.yml"
-      File.open(filepath, 'w+') { |file| file.write(response.to_yaml) }
+      filepath = "#{LOG_LOCATION}#{filename}.yml"
+      File.open(filepath, 'w') { |file| file.write(response.to_yaml) }
     end
   end
 end
--- spec/meetup_api_spec.rb
+++ spec/meetup_api_spec.rb
@@ -5,7 +5,7 @@ require 'yaml'
 require '../lib/meetup_api.rb'
 
 CREDENTIALS = YAML.load(File.read('../config/credentials.yml'))
-@meetup_api = Meetup::MeetupApi.new
+@meetup_api = Meetup::MeetupApi.new(CREDENTIALS)
 cities = @meetup_api.get_cities('tw')['results']
 c = cities[0]
 events = @meetup_api.get_events(c['city'], c['lat'], c['lon'])
