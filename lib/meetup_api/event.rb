285fdf169b764454f621c9551f874fe7fa4e9862
Fabio Daio <fabio-daio@hotmail.com>
2016-10-29 14:15:13 +0800

    Moved library code from lib/ to lib/meetup_api/

---
+++ lib/meetup_api/event.rb
@@ -0,0 +1,37 @@
+# frozen_string_literal: true
+require_relative 'meetup_api'
+require_relative 'location'
+
+module Meetup
+  # Class to set up a Meetup Event
+  class Event
+    attr_reader :name, :status, :city, :venue, :time, :location
+
+    def initialize(name:, status:, city:, venue:, time:, location:)
+      @name = name
+      @status = status
+      @city = city
+      @time = time
+      @venue = venue
+      @location = location
+    end
+  end
+
+  # Class to extract the located events from meetup
+  class LocatedEvents
+    attr_reader :events
+
+    def initialize(meetup_api, location_name:, location:)
+      @meetup_api = meetup_api
+      raw_events = @meetup_api.get_events(location_name,
+                                          location.lat,
+                                          location.lon)
+      @events = raw_events.map do |g|
+        Meetup::Event.new(name: g['name'], status: g['status'],
+                          city: g['venue'] ? g['venue']['city'] : '', # may nil
+                          venue: g['venue'] ? g['venue']['name'] : '', # may nil
+                          time: g['time'], location: location)
+      end
+    end
+  end
+end
---
+++ lib/meetup_api/group.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+require_relative 'meetup_api'
+require_relative 'location'
+
+module Meetup
+  # Class to set up a Meetup Group
+  class Group
+    attr_reader :name
+    attr_reader :urlname
+    attr_reader :city
+    attr_reader :location
+
+    def initialize(name:, urlname:, city:, location:)
+      @name = name
+      @urlname = urlname
+      @city = city
+      @location = location
+    end
+
+    def self.find(meetup_api, urlname:)
+      group = meetup_api.find_group_by_url(urlname)
+      new(name: group['name'],
+          city: group ['city'],
+          urlname: group['urlname'],
+          location: Meetup::Location.new(group['lat'],
+                                         group['lon']))
+    end
+  end
+
+  # Class to extract the located groups from meetup
+  class LocatedGroups
+    attr_reader :groups
+
+    def initialize(meetup_api, country:, location_raw_text:)
+      @meetup_api = meetup_api
+      raw_groups = @meetup_api.get_groups(country, location_raw_text)
+      @groups = raw_groups.map do |g|
+        Meetup::Group.new(
+          name: g['name'], urlname: g['urlname'], city: g['city'],
+          location: Meetup::Location.new(g['lat'], g['lon'])
+        )
+      end
+    end
+  end
+end
